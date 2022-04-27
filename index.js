903dfc8dc29431ffcd4bc9afc4fb0a89950bef9c
Alison Thomson <Alison.Thomson@softwire.com>
2022-04-27 10:31:29 +0100

    refactor to use promise

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const readlineSync = require('readline-sync');
 const request = require('request');
-
-const {getResponse} = require('./requests');
+const reqPromise = require('request-promise-native');
+const {setOptions} = require('./requests');
 const {isValidPostcode} = require('./validation');
 
 let inpPostcode = "";
@@ -17,37 +17,32 @@ while (isValidPostcode(inpPostcode) === false) {
     }
 }
 
-let coords = "";
-let latitude = "";
-let longitude = "";
 const getCoordsUrl = `https://api.postcodes.io/postcodes/${encodeURI(inpPostcode)}`;
-const getCoords = async () => {
-    coords = await getResponse(getCoordsUrl);
-    latitude = coords.result.latitude;
-    longitude = coords.result.longitude;
 
-    const getBusStopsUrl = `https://api.tfl.gov.uk/StopPoint/?lat=${latitude}&lon=${longitude}&stopTypes=NaptanPublicBusCoachTram&radius=1000`
-    const getBusStops = async () => {
-        busStops = await getResponse(getBusStopsUrl);
+reqPromise(setOptions(getCoordsUrl))
+    .then(function (coords)
+    {
+        return getBusStopsUrl = `https://api.tfl.gov.uk/StopPoint/?lat=${coords.result.latitude}&lon=${coords.result.longitude}&stopTypes=NaptanPublicBusCoachTram&radius=1000`;
+    })
+    .then(busStops => reqPromise(setOptions(getBusStopsUrl)))
+    .then(busStops => {
         const nearest2BusStops = busStops.stopPoints.slice(0,2);
         nearest2BusStops.forEach(busStop => {
-            console.log(`Bus stop: ${busStop.commonName}`)
-            const arrivalsUrl = `https://api.tfl.gov.uk/StopPoint/${busStop.naptanId}/Arrivals`; //490009201E
-            let arrivals = [];
-            const getArrivals = async () => {
-                arrivals = await getResponse(arrivalsUrl);
+            console.log(`Bus stop: ${busStop.commonName}`);
+            const arrivalsUrl = `https://api.tfl.gov.uk/StopPoint/${busStop.naptanId}/Arrivals`;
+            reqPromise(setOptions(arrivalsUrl))
+            .then(function(arrivals) {
                 arrivals.sort(function (a, b) {
                     return a.expectedArrival.substring(11, 16).localeCompare(b.expectedArrival.substring(11, 16));
-                });
+                    });
 
                 const firstFiveArrivals = arrivals.slice(0, 5);
                 firstFiveArrivals.forEach(bus => {
                     console.log(`Bus no. ${bus.lineName} to ${bus.destinationName} is arriving at ${bus.expectedArrival.substring(11, 16)}`);
                 })
-            }
-            getArrivals();
+            })
         })
-    }
-    getBusStops();
-}
-getCoords();
+    })
+    .catch(function (err) {
+        console.log(err);
+    });
--- requests.js
+++ requests.js
@@ -1,15 +1,11 @@
 const request = require('request');
 
-exports.getResponse = async function (url) {
-    return new Promise((resolve, reject) => {
-        request(url, function(error, response, body) {
-            if (error) {
-                console.error('error:', error);
-                console.log('statusCode:', response && response.statusCode);
-                reject(error)
-            } else {
-                resolve(JSON.parse(body))
-            }
-        });
-    })
+exports.setOptions = function(url) {
+    return options = {
+        uri: url,
+        headers: {
+            'User-Agent': 'Request-Promise'
+        },
+        json: true
+    };
 }
