a9fc7d090df4a3c3689505b4b5c1e1fa60da5524
Alison Thomson <Alison.Thomson@softwire.com>
2022-04-29 09:15:09 +0100

    rendering 2 busStops with direction

--- frontend/departures.js
+++ frontend/departures.js
@@ -10,9 +10,18 @@ function getDepartures(postcode) {
         if (xhttp.readyState === xhttp.DONE) {
             if (xhttp.status === 200) {
                 var jsonResponse = JSON.parse(xhttp.response);
-                document.getElementById("results").innerHTML = jsonResponse[0].naptanId;
-                console.log("response: " + xhttp.response);
-                console.log("responseText: " + xhttp.responseText);
+                var towards = jsonResponse[0].additionalProperties[1].value
+                document.getElementById("stopName1").innerHTML = `${jsonResponse[0].commonName} towards ${towards}`;
+                var towards = jsonResponse[1].additionalProperties[1].value
+                document.getElementById("stopName2").innerHTML = `${jsonResponse[1].commonName} towards ${towards}`;
+                var str = "<ul>"
+                var obj = jsonResponse[0].additionalProperties[1];
+                for (var key in obj){
+                    str += `<li>${key} is ${obj[key]}</li>`;
+                }
+
+                document.getElementById("fullResponse").innerHTML = str;
+
             } else {
                 console.log(`error? ${xhttp.status}`);
             }
